5f68bd000044921694cfb25fb1dd47308eb9fc8c
Camille <camille.almeida@padolabs.com.br>
2022-03-14 16:11:31 -0300

    Server Put

--- src/server.js
+++ src/server.js
@@ -6,9 +6,11 @@ server.get('/device', (req, res) => {
     res.send('Hello world')
 })
 server.post('/device', function (req, res) {
-    res.send('Got a POST request');
+    res.send('Got a POST request')
   })
-  
+server.put('/device', function (req, res) {
+    res.send('Got a PUT request at /user')
+  })
 
 server.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
