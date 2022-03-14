eb47c5a17fbb2d1ca1fcb20ddd754a6488899f47
Camille <camille.almeida@padolabs.com.br>
2022-03-14 16:08:55 -0300

    Server Post

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const port = 3000
 server.get('/device', (req, res) => {
     res.send('Hello world')
 })
+server.post('/device', function (req, res) {
+    res.send('Got a POST request');
+  })
+  
 
 server.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
